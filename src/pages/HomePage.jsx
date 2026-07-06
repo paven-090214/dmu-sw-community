683b048455dd4d5003c2985c695ed0f5fac1aa94
SuHwan An <127846128+paven-090214@users.noreply.github.com>
2026-07-06 19:46:52 +0900

    Add files via upload

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/header";
+import AcademyList from "../features/academies/AcademyList";
+
+function HomePage (){
+    return(
+        <div>
+        </div>
+    );
+}
+export default HomePage;
