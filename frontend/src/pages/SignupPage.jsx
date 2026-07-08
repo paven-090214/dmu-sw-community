3d588e659ee2cd6a16a53a63aa1145e475f4731e
AnSuHwan <yj6042yj@gmail.com>
2026-07-09 01:19:00 +0900

    add board pages and routing

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,13 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 export default function SignupPage() { 
+    const history = useNavigate();
     return (
         <>
             <h1>회원가입</h1><hr />
             <form method="post">
                 닉네임 : <input type="text" /><br />
                 아이디 : <input type="text" /><br />
-                비밀번호 : <input type="text" /><br />
+                비밀번호 : <input type="password" /><br />
                 <button type="button">회원가입</button>
-                <button type="button">돌아가기</button>
+                <button type="button" onClick={()=> history(-1)}>돌아가기</button>
             </form>
         </>
     );
--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,16 +1,22 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 import MainPage from '../pages/MainPage';
 import Header from '../components/header/Header';
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
+import BoardPage from '../pages/BoardPage';
+import BoardWritePage from '../pages/BoardWritePage';
 
 function Router(){
     return(
         <Routes>
+            <Route path="/" element={<Navigate to="/mainpage" replace />} />
             <Route path="/mainpage" element={<MainPage />} />
             <Route path="/signuppage" element={<SignupPage />}/>
             <Route path="/loginpage" element={<LoginPage />}/>
+            <Route path="/board/:boardType" element={<BoardPage />} />
+            <Route path="/board/:boardType/write" element={<BoardWritePage />} />
         </Routes>
     )
 }
