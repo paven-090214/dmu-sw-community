d0e1dec45bd4e519c1b5d4a1f98cfc07d0aacb87
AnSuHwan <yj6042yj@gmail.com>
2026-07-15 08:02:58 +0900

    add boardList

--- frontend/src/pages/BoardPage.jsx
+++ frontend/src/pages/BoardPage.jsx
@@ -1,10 +1,30 @@
 import {useParams, Link, useNavigate} from 'react-router-dom';
 import {boardConfig} from '../constants/boardConfig';
+import { useState, useEffect } from 'react';
 
 export default function BoardPage(){
     const {boardType} = useParams();
     const config = boardConfig[boardType];
     const history = useNavigate();
+    const [boardList, setBoardList] = useState([]);
+
+    useEffect(()=>{
+        const getBoardList = async()=> {
+            try{
+                const response = await fetch(`/api/board/${boardType}`);
+                if(!response.ok) {
+                    throw new Error("게시글 조회에 실패했습니다.");
+                }
+
+                const data = await response.json();
+                setBoardList(data);
+            } catch(e) {
+                console.error(e);
+                alert("게시글 조회에 실패했습니다.");
+            }
+        };
+        getBoardList();
+    },[boardType]);
 
     if(!config) {
         return(
@@ -18,7 +38,20 @@ export default function BoardPage(){
     return(
         <div>
             <h1>{config.title}</h1><hr />
-            <p>{config.description}</p>
+            {boardList.length === 0 ? (
+                <h2>작성된 글이 없습니다.</h2>
+            ) : (
+                <div>
+                    {boardList.map((board)=>(
+                        <div key={board.bNumber}>
+                            <h3>{board.title}</h3>
+                            <span>작성자 : {board.nickname}</span>
+                            <span>조회수 : {board.viewCount}</span>
+                            <span>작성일 : {board.createdAt}</span>
+                        </div>
+                    ))}
+                </div>
+            )}
             {config.canWrite && (
                 <Link to={`/board/${boardType}/write`}><button>글 작성</button></Link>
             )}
